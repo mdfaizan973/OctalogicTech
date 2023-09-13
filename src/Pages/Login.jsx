4dfb3917d3071a53c3c1bf17ad63fdaa39b54555
mdfaizan973 <faizan.md9735@gmail.com>
2023-09-13 20:04:39 +0530

    aa

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -36,7 +36,7 @@ export default function Login() {
                     name="email"
                     id="email"
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="name@company.com"
+                    placeholder="Enter Email...."
                     required=""
                   />
                 </div>
@@ -48,7 +48,7 @@ export default function Login() {
                     type="password"
                     name="password"
                     id="password"
-                    placeholder="••••••••"
+                    placeholder="Enter Password"
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     required=""
                   />
